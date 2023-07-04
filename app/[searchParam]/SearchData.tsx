43d6066ad5609ce9afe622ce4b903f3969e4dbf8
proy13 <paruyryeprikyan9@gmail.com>
2023-07-04 13:12:34 +0400

    added accordion and external links for source tx and destination tx, also CopyClipboard

--- app/[searchParam]/SearchData.tsx
+++ app/[searchParam]/SearchData.tsx
@@ -2,6 +2,9 @@
 
 import { shortenAddress } from "@/lib/utils";
 import { ApiResponse } from "@/models/ApiResponse";
+import CopyButton from "../../components/buttons/copyButton";
+import { ExternalLink } from 'lucide-react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../../components/shadcn/accordion';
 import useSWR from "swr"
 
 type Swap = {
@@ -48,36 +51,174 @@ export default function SearchData({ searchParam }: { searchParam: string }) {
             </div>
             {swap && <div className="border-t border-secondary-500">
                 <dl className="divide-y divide-secondary-400">
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-white">Date</dt>
                         <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">{new Date(swap?.created_date).toLocaleString()}</dd>
                     </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-white">Status</dt>
                         <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">{swap.status}</dd>
                     </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-white">To Address</dt>
                         <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">{swap.destination_address}</dd>
                     </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-white">Asset</dt>
                         <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">{swap.destination_network_asset}</dd>
                     </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-white">Source Tx</dt>
-                        <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">
-                            {swap.input_transaction.transaction_id}
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-white">Destination Tx</dt>
-                        <dd className="mt-1 text-sm leading-6 text-primary-text sm:col-span-2 sm:mt-0">
-                            {swap.output_transaction.transaction_id}
-                        </dd>
-                    </div>
+
+                    <Accordion type="single" collapsible>
+                        <AccordionItem value={'item-1'}>
+                            <AccordionTrigger className="items-center flex w-full relative gap-2 rounded-lg text-left text-base font-medium accordion relative">
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white">Source Tx</dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <a href={`${swap?.input_transaction?.explorer_url}`} target="_blank" className="hover:text-gray-300 inline-flex items-center w-fit">
+                                            {swap.input_transaction.transaction_id}
+                                            <ExternalLink width={16} height={16} className="ml-1" />
+                                        </a>
+                                    </dd>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="text-sm text-primary-text font-normal">
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Amount:</span> {swap?.input_transaction?.amount}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Confirmations:</span> {`${swap?.input_transaction?.confirmations}/${swap?.input_transaction?.max_confirmations}`}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Created date:</span> {new Date(swap?.input_transaction?.created_date).toLocaleString()}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">From:</span>
+                                        <CopyButton toCopy={swap?.input_transaction?.from} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {shortenAddress(swap?.input_transaction?.from)}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">To:</span>
+                                        <CopyButton toCopy={swap?.input_transaction?.to} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {swap?.input_transaction?.to}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">Transaction id:</span>
+                                        <CopyButton toCopy={swap?.input_transaction?.transaction_id} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {shortenAddress(swap?.input_transaction?.transaction_id)}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">USD price:</span> {swap?.input_transaction?.usd_price}
+                                    </dd>
+                                </div>
+                                <div className="px-4 pt-2 pb-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">USD value:</span> {swap?.input_transaction?.usd_value}
+                                    </dd>
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+
+                    <Accordion type="single" collapsible>
+                        <AccordionItem value={'item-1'}>
+                            <AccordionTrigger className="items-center flex w-full relative gap-2 rounded-lg text-left text-base font-medium accordion relative">
+                                <div className="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white">Destination Tx</dt>
+                                    <a href={`${swap?.output_transaction?.explorer_url}`} target="_blank" className="hover:text-gray-300 inline-flex items-center w-fit">
+                                        {swap.output_transaction.transaction_id}
+                                        <ExternalLink width={16} height={16} className="ml-1" />
+                                    </a>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="text-sm text-primary-text font-normal">
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Amount:</span> {swap?.output_transaction?.amount}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Confirmations:</span> {`${swap?.output_transaction?.confirmations}/${swap?.output_transaction?.max_confirmations}`}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">Created date:</span> {new Date(swap?.output_transaction?.created_date).toLocaleString()}
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">From:</span>
+                                        <CopyButton toCopy={swap?.output_transaction?.from} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {shortenAddress(swap?.output_transaction?.from)}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">To:</span>
+                                        <CopyButton toCopy={swap?.output_transaction?.to} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {swap?.output_transaction?.to}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0 flex">
+                                        <span className="text-white mr-1">Transaction id:</span>
+                                        <CopyButton toCopy={swap?.output_transaction?.transaction_id} iconHeight={22} iconClassName="order-2" iconWidth={22}>
+                                            {shortenAddress(swap?.output_transaction?.transaction_id)}
+                                        </CopyButton>
+                                    </dd>
+                                </div>
+                                <div className="px-4 py-2 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">USD price:</span> {swap?.output_transaction?.usd_price}
+                                    </dd>
+                                </div>
+                                <div className="px-4 pt-2 pb-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+                                    <dt className="text-sm font-medium text-white"></dt>
+                                    <dd className="mt-1 text-sm leading-6 text-primary-text sm:mt-0">
+                                        <span className="text-white">USD value:</span> {swap?.output_transaction?.usd_value}
+                                    </dd>
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
                 </dl>
-            </div>}
-        </div>
+            </div >}
+        </div >
     )
 }
