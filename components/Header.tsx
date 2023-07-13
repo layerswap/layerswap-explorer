9c44a292f01c8a5ad3265081803307b1bdc0323c
proy13 <paruyr@bransfer.io>
2023-07-13 13:29:07 +0400

    back button place changed, some style changes in cards

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { useState } from 'react'
 import LayerSwapLogo from './icons/layerSwapLogo'
 import Search from './Search'
 import { usePathname } from 'next/navigation'
-import BackBtn from '@/helpers/BackButton'
 import Link from 'next/link'
 
 const navigation = [
@@ -13,13 +11,10 @@ const navigation = [
 ]
 
 export default function Header() {
-    const pathname = usePathname()
+    const pathname = usePathname();
 
     return (
         <header className="max-w-6xl w-full mx-auto">
-            {pathname !== '/' && <div className='hidden xl:block absolute left-4 top-4 md:left-8 md:top-8 hover:bg-secondary-600 hover:text-accent-foreground rounded ring-offset-background transition-colors'>
-                <BackBtn />
-            </div>}
             <nav className={`mx-auto max-w-6xl grid grid-cols-2 lg:grid-cols-6 lg:grid-rows-1 items-center py-6 px-6 lg:px-8 ${pathname !== '/' ? 'grid-rows-2' : 'grid-rows-1'}`} aria-label="Global">
                 <Link href="/" className="-m-1.5 p-1.5 order-1 col-span-1">
                     <LayerSwapLogo className="h-14 w-auto text-primary-logoColor" />
