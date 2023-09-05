02d3af1c4e2394a72e9632aa17033b3ab703b201
Aren <aren@bransfer.io>
2023-09-05 18:10:15 +0400

    add version query in api calls

--- context/settings.tsx
+++ context/settings.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { mapNetworkCurrencies } from '@/helpers/settingsHelper';
+
 import LayerSwapApiClient from '@/lib/layerSwapApiClient';
 import { ApiResponse } from '@/models/ApiResponse';
 import { LayerSwapAppSettings } from '@/models/LayerSwapAppSettings';
 import { LayerSwapSettings } from '@/models/LayerSwapSettings';
-import React, { FC, ReactNode, useEffect, useState } from 'react'
+import React, { FC, ReactNode } from 'react'
 import useSWR from 'swr';
 
 const SettingsStateContext = React.createContext<LayerSwapAppSettings | null>(null);
@@ -13,7 +13,7 @@ export const SettingsProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
   const fetcher = (url: string) => fetch(url).then(r => r.json());
   const version = process.env.NEXT_PUBLIC_API_VERSION;
-  const { data: settings, error, isLoading } = useSWR<ApiResponse<LayerSwapSettings>>(`${LayerSwapApiClient.apiBaseEndpoint}/api/settings?version=${version}`, fetcher, { dedupingInterval: 60000 });
+  const { data: settings } = useSWR<ApiResponse<LayerSwapSettings>>(`${LayerSwapApiClient.apiBaseEndpoint}/api/settings?version=${version}`, fetcher, { dedupingInterval: 60000 });
 
   let appSettings = new LayerSwapAppSettings(settings?.data);
 
