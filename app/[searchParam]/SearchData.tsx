c6b61c630810f322ea8d8e35be9967d72efee6a7
proy13 <paruyr@bransfer.io>
2023-11-14 15:29:12 +0400

    fixed row external link

--- app/[searchParam]/SearchData.tsx
+++ app/[searchParam]/SearchData.tsx
@@ -135,7 +135,7 @@ export default function SearchData({ searchParam }: { searchParam: string }) {
                                             return
 
                                         return (
-                                            <tr key={index} onClick={() => router.push(`/${input_transaction?.transaction_id}`)} className="hover:bg-secondary-600 hover:cursor-pointer">
+                                            <tr key={index} onClick={(e) => router.push(`/${inputTransaction?.transaction_id}`)} className="hover:bg-secondary-600 hover:cursor-pointer">
                                                 <td className="whitespace-nowrap py-2 px-3 text-sm font-medium text-white flex flex-col">
                                                     <Link href={`/${inputTransaction?.transaction_id}`} onClick={(e) => e.stopPropagation()} className="hover:text-gray-300 inline-flex items-center w-fit">
                                                         {shortenAddress(inputTransaction?.transaction_id)}
