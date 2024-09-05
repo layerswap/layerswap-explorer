0fb41328519074bc97d83e47384f17f1b8a5c8f6
paur94 <paruyryeprikyan9@gmail.com>
2024-09-05 19:08:51 +0400

    fixed symbol issue

--- components/notFound.tsx
+++ components/notFound.tsx
@@ -6,9 +6,9 @@ export default function NotFound() {
         <section className="fixed inset-0 flex items-center justify-center">
             <NotFoundBackground className="absolute"/>
             <div className="flex flex-col items-center justify-center text-center p-4 relative z-20">
-                <h1 className="text-2xl font-semibold text-white md:text-4xl">We Couldn't find any logs</h1>
+                <h1 className="text-2xl font-semibold text-white md:text-4xl">We couldn&#39;t find any logs</h1>
                 <p className="text-base text-primary-text-placeholder mt-2">Please make sure you entered a valid address/source Tx/destination TX.</p>
-                <span className="text-base text-primary-text-placeholder block">if the issue persists, you can contact our support.</span>
+                <span className="text-base text-primary-text-placeholder block">If the issue persists, you can contact our support.</span>
                 <div className="flex items-center mt-6 gap-x-3">
                     <Link href={'/'} className="w-full px-5 py-2 text-sm tracking-wide text-primary-text transition-colors duration-200 bg-secondary-600 rounded-lg shrink-0 sm:w-auto hover:bg-secondary-700/80">
                         Clear search
@@ -16,5 +16,5 @@ export default function NotFound() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
