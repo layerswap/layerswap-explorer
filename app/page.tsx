a268c48143ce2e0f736cbc30985cf25b58ac5284
paur94 <paruyryeprikyan9@gmail.com>
2024-09-17 13:07:04 +0400

    removed search padding

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <main className="w-full pb-5 px-6 xl:px-0 h-full flex flex-col">
-      <div className="mx-auto w-full px-6 lg:px-8 flex flex-col">
+      <div className="mx-auto w-full lg:px-8 flex flex-col">
         <Search />
       </div>
       <DataTable />
