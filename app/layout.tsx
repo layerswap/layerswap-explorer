32957d9f104fbcd6fb434959e169308c611829dd
paur94 <paruyryeprikyan9@gmail.com>
2024-10-31 19:37:56 +0400

    added slash after urls

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
       <Head>
         <title>Layerswap Explorer</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="canonical" href="https://layerswap.io/explorer" />
+        <link rel="canonical" href="https://layerswap.io/explorer/" />
         <meta name="description" content="Layerswap Explorer provides a detailed view of all transactions going through Layerswap. Search by address or transaction hash to get the information you need." />
       </Head>
       <Script defer data-domain="layerswap.io" src="https://plausible.io/js/script.js" />
