d561ff9b97343d23ec6dda2fe8c8e75e39fb1448
paur94 <paruyryeprikyan9@gmail.com>
2024-11-12 19:50:29 +0400

    fixed metadata section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,17 @@ import Header from '@/components/Header'
 import './globals.css'
 import Footer from '@/components/Footer'
 import Script from 'next/script'
-import Head from 'next/head'
+
+export const metadata = {
+  title: 'Layerswap Explorer',
+  description: 'Layerswap Explorer provides a detailed view of all transactions going through Layerswap. Search by address or transaction hash to get the information you need.',
+  icons: {
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  alternates: {
+    canonical: 'https://layerswap.io/explorer/',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -11,18 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Layerswap Explorer</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="canonical" href="https://layerswap.io/explorer/" />
-        <meta name="description" content="Layerswap Explorer provides a detailed view of all transactions going through Layerswap. Search by address or transaction hash to get the information you need." />
-      </Head>
-      <Script defer data-domain="layerswap.io" src="https://plausible.io/js/script.js" />
       <body className='flex min-h-screen flex-col items-center max-w-6xl mx-auto'>
         <Header />
         {children}
         <Footer />
+        <Script defer data-domain="layerswap.io" src="https://plausible.io/js/script.js" />
       </body>
     </html>
-  )
+  );
 }
